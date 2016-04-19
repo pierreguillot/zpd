1a141299508fa5a38e9ae235b22bcb96ac972ee3
pierreguillot <guillotpierre6@gmail.com>
2016-04-19 13:11:57 +0200

    try atom

--- xpd/xpd_atom.hpp
+++ xpd/xpd_atom.hpp
@@ -26,55 +26,60 @@ namespace xpd
         //! @details For the moment it can be null_t, float_t or symbol_t
 #if (__cplusplus <= 199711L)
         enum type_t
-#else
-        enum class type_t
-#endif
         {
             null_t,     //!< @brief The atom is null or undefined.
             float_t,    //!< @brief The atom owns a float value.
             symbol_t    //!< @brief The atom owns a symbol.
         };
+#else
+        enum class type_t
+        {
+            null_t,     //!< @brief The atom is null or undefined.
+            float_t,    //!< @brief The atom owns a float value.
+            symbol_t    //!< @brief The atom owns a symbol.
+        };
+#endif
         
         //! @brief The default constructor.
         //! @details Creates an null atom.
-        inline xpd_constexpr atom() xpd_noexcept : m_type(atom::type_t::null_t), m_word(0.f) {}
+        inline xpd_constexpr atom() xpd_noexcept : m_type(null_t), m_word(0.f) {}
         
         //! @brief The float constructor.
         //! @details Creates an float atom.
-       inline xpd_constexpr atom(xpd::float_t value) xpd_noexcept : m_type(atom::type_t::float_t), m_word(value) {}
+       inline xpd_constexpr atom(float value) xpd_noexcept : m_type(float_t), m_word(value) {}
         
         //! @brief The symbol constructor.
         //! @details Creates an symbol atom.
-       inline xpd_constexpr atom(symbol& symbol) xpd_noexcept : m_type(atom::type_t::symbol_t), m_word(symbol) {}
+       inline xpd_constexpr atom(symbol& symbol) xpd_noexcept : m_type(symbol_t), m_word(symbol) {}
         
         //! @brief The copy constructor.
         //! @details Creates an copy of another atom.
-       inline xpd_constexpr atom(atom& other) xpd_noexcept : m_type(other.m_type), m_word(m_type == atom::type_t::float_t ? other.m_word.w_float : other.m_word.w_symbol) {}
+       inline xpd_constexpr atom(atom& other) xpd_noexcept : m_type(other.m_type), m_word(m_type == float_t ? other.m_word.w_float : other.m_word.w_symbol) {}
         
         //! @brief The float assignment.
         //! @details Sets the atom to a new float value.
         //! @param value The float value.
         //! @return The reference of the atom.
-        inline atom& operator=(xpd::float_t value) xpd_noexcept {m_type = atom::type_t::float_t; m_word.w_float = value; return *this;}
+        inline atom& operator=(float value) xpd_noexcept {m_type = float_t; m_word.w_float = value; return *this;}
         
         //! @brief The symbol assignment.
         //! @details Sets the atom to a new symbol.
         //! @param symbol The symbol value.
         //! @return The reference of the atom.
-        inline atom& operator=(symbol& symbol) xpd_noexcept {m_type = atom::type_t::symbol_t; m_word.w_symbol = symbol; return *this;}
+        inline atom& operator=(symbol& symbol) xpd_noexcept {m_type = symbol_t; m_word.w_symbol = symbol; return *this;}
         
         //! @brief The copy assignment.
         //! @details Returns a copy of another atom.
         //! @param other The other atom.
         //! @return The reference of the atom.
-        inline atom& operator=(atom& other) {m_type = other.m_type; m_type == atom::type_t::float_t ? m_word.w_float = other.m_word.w_float : m_word.w_symbol = other.m_word.w_symbol; return *this;}
+        inline atom& operator=(atom& other) {m_type = other.m_type; m_type == float_t ? m_word.w_float = other.m_word.w_float : m_word.w_symbol = other.m_word.w_symbol; return *this;}
         
         
         //! @brief Gets the float value of the atom.
         //! @details Returns the float value of the atom if the type if float_t otherwise the
         //! behavior is undefined.
         //! @return The float value of the atom.
-       inline xpd_constexpr operator xpd::float_t() const xpd_noexcept {return m_word.w_float;}
+       inline xpd_constexpr operator float() const xpd_noexcept {return m_word.w_float;}
         
         //! @brief Gets the symbol of the atom.
         //! @details Returns the symbol of the atom if the type if symbol_t otherwise the
@@ -91,10 +96,10 @@ namespace xpd
 #if (__cplusplus <= 199711L)
         struct a_word
         {
-            xpd::float_t w_float;
+            float w_float;
             symbol  w_symbol;
            inline xpd_constexpr a_word() xpd_noexcept : w_float(0.f) {};
-           inline xpd_constexpr a_word(const xpd::float_t value) xpd_noexcept : w_float(value) {}
+           inline xpd_constexpr a_word(const float value) xpd_noexcept : w_float(value) {}
            inline xpd_constexpr a_word(symbol& symbol) xpd_noexcept : w_symbol(symbol) {}
             
         };
@@ -109,7 +114,7 @@ namespace xpd
             
         };
 #endif
-        atom::type_t m_type;
+        type_t m_type;
         a_word m_word;
     };
 }
