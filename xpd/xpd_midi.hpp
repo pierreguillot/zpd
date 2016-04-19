9a5c7862ca5b08ae3f043a5519094f2271e54ac0
pierreguillot <guillotpierre6@gmail.com>
2016-04-19 13:13:17 +0200

    and midi

--- xpd/xpd_midi.hpp
+++ xpd/xpd_midi.hpp
@@ -26,7 +26,7 @@ namespace xpd
             //! @brief The available type of midi events.
             //! @details The type is used .
 #if (__cplusplus <= 199711L)
-            typedef enum type_
+            enum type_t
             {
                 note_t            = 0,   //!< @brief The note (in or out) event.
                 control_change_t  = 1,   //!< @brief The control change event.
@@ -35,7 +35,7 @@ namespace xpd
                 after_touch_t     = 4,   //!< @brief The after touch event.
                 poly_after_touch_t= 5,   //!< @brief The poly after touch event.
                 byte_t            = 6    //!< @brief The byte event.
-            } type_t;
+            };
 #else
             enum class type_t : int
             {
@@ -50,39 +50,39 @@ namespace xpd
 #endif
             
             //! @brief The event raw constructor.
-            inline xpd_constexpr event(event::type_t t, int v1, int v2, int v3) xpd_noexcept :
+            inline xpd_constexpr event(type_t t, int v1, int v2, int v3) xpd_noexcept :
             m_type(t), m_val1(v1), m_val2(v2), m_val3(v3) {}
             
             //! @brief The note event constructor.
             static inline xpd_constexpr event note(int channel, int pitch, int velocity) xpd_noexcept
-            {return event(event::type_t::note_t, channel, pitch, velocity);}
+            {return event(note_t, channel, pitch, velocity);}
             
             //! @brief The control change event constructor.
             static inline xpd_constexpr event control_change(int channel, int controler, int value) xpd_noexcept
-            {return event(event::type_t::control_change_t, channel, controler, value);}
+            {return event(control_change_t, channel, controler, value);}
             
             //! @brief The program change event constructor.
             static inline xpd_constexpr event program_change(int channel, int program) xpd_noexcept
-            {return event(event::type_t::control_change_t, channel, program, 0);}
+            {return event(control_change_t, channel, program, 0);}
             
             //! @brief The pitch bend event constructor.
             static inline xpd_constexpr event pitch_bend(int channel, int bend) xpd_noexcept
-            {return event(event::type_t::pitch_bend_t, channel, bend, 0);}
+            {return event(pitch_bend_t, channel, bend, 0);}
             
             //! @brief The pitch bend event constructor.
             static inline xpd_constexpr event after_touch(int channel, int value) xpd_noexcept
-            {return event(event::type_t::after_touch_t, channel, 0, value);}
+            {return event(after_touch_t, channel, 0, value);}
             
             //! @brief The pitch bend event constructor.
             static inline xpd_constexpr event poly_after_touch(int channel, int pitch, int value) xpd_noexcept
-            {return event(event::type_t::after_touch_t, channel, pitch, value);}
+            {return event(after_touch_t, channel, pitch, value);}
             
             //! @brief The pitch bend event constructor.
             static inline xpd_constexpr event byte(int value) xpd_noexcept
-            {return event(event::type_t::after_touch_t, 0, 0, value);}
+            {return event(after_touch_t, 0, 0, value);}
             
             //! @brief Gets the type of the event.
-           inline xpd_constexpr event::type_t type() const xpd_noexcept {return m_type;}
+           inline xpd_constexpr type_t type() const xpd_noexcept {return m_type;}
             
             //! @brief Gets the channel value.
            inline xpd_constexpr int channel() const xpd_noexcept {return m_val1;}
@@ -109,7 +109,7 @@ namespace xpd
            inline xpd_constexpr int after_touch() const xpd_noexcept {return m_val3;}
             
         private:
-            event::type_t m_type;
+            type_t m_type;
             int         m_val1;
             int         m_val2;
             int         m_val3;
