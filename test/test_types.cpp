a114c4937be3e5abfff32958954a96908cc7a1d1
pierreguillot <guillotpierre6@gmail.com>
2016-06-12 15:40:30 +0200

    update license date

--- test/test_types.cpp
+++ test/test_types.cpp
@@ -1,25 +1,8 @@
 /*
- ==============================================================================
- 
- This file is part of the KIWI library.
- Copyright (c) 2014 Pierre Guillot & Eliott Paris.
- 
- Permission is granted to use this software under the terms of either:
- a) the GPL v2 (or any later version)
- b) the Affero GPL v3
- 
- Details of these licenses can be found at: www.gnu.org/licenses
- 
- KIWI is distributed in the hope that it will be useful, but WITHOUT ANY
- WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
- 
- ------------------------------------------------------------------------------
- 
- To release a closed-source product which uses KIWI, contact : guillotpierre6@gmail.com
- 
- ==============================================================================
- */
+ // Copyright (c) 2015-2016-2016 Pierre Guillot.
+ // For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ // WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+*/
 
 
 #include "../xpd/xpd.hpp"
