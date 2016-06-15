b771f43fde49f43eb6b580085991dcf672176314
Pierre <guillotpierre6@gmail.com>
2016-06-15 18:52:59 +0200

    move oxygen files

---
+++ config/publish-doxygen.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd config
+rm -rf doc
+mkdir -p doc
+git clone -b gh-pages git@github.com:pierreguillot/zpd.git --single-branch doc
+
+
+cd doc
+git rm -rf .
+cd ..
+doxygen xpddoc
+cd ..
+doxygen cpddoc
+cp doxygen.css doc/doxygen.css
+cp index.html doc/index.html
+
+
+cd doc
+git add .
+git commit -m "Automated documentation build."
+git push origin gh-pages
+cd ..
