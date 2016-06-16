5ce001344c67f1078d203528e96dc409b3392332
Pierre <guillotpierre6@gmail.com>
2016-06-16 12:11:23 +0200

    ça

--- config/publish-release.sh
+++ config/publish-release.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "Publish release..."
 mkdir zpd
 cp -R lib zpd/lib
 cp -R cpd zpd/cpd
@@ -9,6 +8,8 @@ cp README.md zpd/readme.txt
 cp LICENSE.txt zpd/license.txt
 cd zpd
 ls
+echo $TRAVIS_OS_NAME
+echo $ZPD_ARCH
+echo $TRAVIS_TAG
 echo zpd_$TRAVIS_OS_NAME_$ZPD_ARCH_$TRAVIS_TAG
 zip -r zpd_$TRAVIS_OS_NAME_$ZPD_ARCH_$TRAVIS_TAG.zip zpd
-echo "Done"
