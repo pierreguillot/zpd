ce43c6847572e6cd2649b26bca1472a5c838fcbd
Pierre <guillotpierre6@gmail.com>
2016-06-16 15:12:39 +0200

    correct publish release

--- config/publish-release.sh
+++ config/publish-release.sh
@@ -6,10 +6,5 @@ cp -R cpd zpd/cpd
 cp -R xpd zpd/xpd
 cp README.md zpd/readme.txt
 cp LICENSE.txt zpd/license.txt
-cd zpd
-ls
-echo $TRAVIS_OS_NAME
-echo $ZPD_ARCH
-echo $TRAVIS_TAG
-echo zpd_$TRAVIS_OS_NAME_$ZPD_ARCH_$TRAVIS_TAG
-zip -r zpd_$TRAVIS_OS_NAME_$ZPD_ARCH_$TRAVIS_TAG.zip zpd
+
+zip -r zpd_${TRAVIS_OS_NAME}_${ZPD_ARCH}_${TRAVIS_TAG}.zip zpd
