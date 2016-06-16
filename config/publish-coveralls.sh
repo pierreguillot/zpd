aa21398bbccd7aff8dc57204bbdd1735efada409
Pierre <guillotpierre6@gmail.com>
2016-06-16 16:44:11 +0200

    test ça

--- config/publish-coveralls.sh
+++ config/publish-coveralls.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $ZPD_COV == 'On' ]
+if [ $ZPD_COV == 'on' ]
 then
 coveralls -e build/CMakeFiles/feature_tests.cxx -e build/CMakeFiles/feature_tests.c -e build/CMakeFiles/CompilerIdCXX/CMakeCXXCompilerId.cpp -e build/CMakeFiles/CompilerIdCXX/CMakeCXXCompilerId.c -e build/CMakeFiles/3.2.2/CompilerIdCXX/CMakeCXXCompilerId.cpp -e build/CMakeFiles/3.2.2/CompilerIdC/CMakeCCompilerId.c -e test -e pd -e thread --gcov-options '\-lp'
 fi
